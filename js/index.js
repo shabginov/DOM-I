fb48df03302f8e3f4e0acfe76be829ea09ac1945
Alexey Shabginov <a.shabginov@gmail.com>
2020-03-11 21:36:01 -0700

    Implemented header

--- js/index.js
+++ js/index.js
@@ -37,6 +37,31 @@ const siteContent = {
   },
 };
 
+// Navbar
+const navLinks = document.querySelectorAll('a');
+const services = navLinks[0].textContent = siteContent.nav['nav-item-1'];
+const product = navLinks[1].textContent = siteContent.nav['nav-item-2'];
+const vision = navLinks[2].textContent = siteContent.nav['nav-item-3'];
+const features = navLinks[3].textContent = siteContent.nav['nav-item-4'];
+const about = navLinks[4].textContent = siteContent.nav['nav-item-5'];
+const contact = navLinks[5].textContent = siteContent.nav['nav-item-6'];
+
+//Creating and Appending elements
+const prependedA = document.createElement('a');
+const appendedA = document.createElement('a');
+prependedA.textContent = 'Prepended';
+appendedA.textContent = 'Appended';
+document.querySelector('nav').prepend(prependedA);
+document.querySelector('nav').append(appendedA);
+
+// Changing font color to green
+// navLinks.style.color = 'green';
+
+// Looping through a tags
+// for (let i = 0; i < navLinks.length; i++) {
+
+// }
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])

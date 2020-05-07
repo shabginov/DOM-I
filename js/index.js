38454922371420151d2c8f1037a2114275fe2f59
Alexey Shabginov <a.shabginov@gmail.com>
2020-05-06 19:22:51 -0700

    Completes MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,81 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav elements
+const links = document.querySelectorAll("a");
+links[0].textContent = siteContent.nav["nav-item-1"];
+links[1].textContent = siteContent.nav["nav-item-2"];
+links[2].textContent = siteContent.nav["nav-item-3"];
+links[3].textContent = siteContent.nav["nav-item-4"];
+links[4].textContent = siteContent.nav["nav-item-5"];
+links[5].textContent = siteContent.nav["nav-item-6"];
+// siteContent.forEach(item) => nav.textContent = nav.item;
+
+// cta elements
+const h1 = document.querySelector('.cta-text h1');
+h1.textContent = siteContent.cta.h1;
+
+const button = document.querySelector('.cta-text button');
+button.textContent = siteContent.cta.button;
+
+const ctaImg = document.querySelector('.cta img');
+ctaImg.src = siteContent.cta["img-src"];
+
+// main content
+const h4 = document.querySelectorAll('.text-content h4');
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const p = document.querySelectorAll('.text-content p');
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
+
+const middleImg = document.getElementById('middle-img');
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+// contact elements
+const h4Contact = document.querySelector('section.contact h4')
+h4Contact.textContent = siteContent.contact["contact-h4"];
+
+const pContact = document.querySelectorAll('section.contact p');
+pContact[0].textContent = siteContent.contact.address;
+pContact[1].textContent = siteContent.contact.phone;
+pContact[2].textContent = siteContent.contact.email;
+
+// footer
+const footerP = document.querySelector('footer p');
+console.log(footerP);
+footerP.textContent = siteContent.footer.copyright;
+
+// changing nav items to green
+links.forEach(element => element.style.color = 'green');
+
+// creating and adding elements to DOM
+
+const prependedElement = document.createElement('a');
+prependedElement.textContent = 'Prepended';
+
+const appendedElement = document.createElement('a');
+appendedElement.textContent = 'Appended';
+
+const nav = document.querySelector('nav');
+nav.prepend(prependedElement);
+nav.append(appendedElement)
+
+// stretch
+const changeButton = document.createElement('button');
+changeButton.textContent = 'Change background';
+const divCta = document.querySelector('div.cta-text');
+console.log(divCta);
+divCta.append(changeButton);
+const body = document.querySelector('body');
+console.log(body);
+
+changeButton.addEventListener('click', () => body.style.backgroundColor = 'blue');
